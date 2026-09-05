197c9a0743d6688b47e00c3718a0e10396bacc8c
mthsaar <matheussaar@gmail.com>
2026-09-04 21:52:51 -0300

    Add project files

---
+++ atv.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    
+    //cadastro
+
+    //carrinho de compra
+    
+    
+    //pagamento
+    int pgto
+    int v1
+    char cupom
+
+    printf("O valor total da compra foi de %d reais", v1)
+    printf("Você possui cupom de desconto? S/N")
+    scanf(cupom)
+    if (cupom == "PROMO10")
+    {
+       v2 = v1 - (v1*0.10)
+       printf("O valor ")
+    } else { 
+        v2
+    
+    printf("Selecione a forma de pagamento: 1 - Pix, 2 - Débito, 3 - Crédito")
+    scanf(pgto)
+
+    //relatório
+
+
+
+return 0;
+}
