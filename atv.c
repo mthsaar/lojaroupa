f66ca43b6cd680048c86f87d7bc322ee5a39f069
mthsaar <matheussaar@gmail.com>
2026-09-10 20:27:57 -0300

    código indentado, funcao stringcmp implementada

--- atv.c
+++ atv.c
@@ -30,32 +30,19 @@ int main()
     //Sistema de login
     
     while(1) {
-    
-    printf("\nLogin:\n ");
-    
-    printf("\nEmail: ");
-    scanf("%99s", email);
-    
-    printf("Senha: ");
-    scanf("%d", &senha);
-    
-    //verificador login
-    
-    
-    if (strcmp(email, emailcad)==0 && senha == senhacad) {
-        printf("Seus dados estão corretos!");
-        
-        break;
-    }    
-        
-    else {
+        printf("\nLogin:\n ");
+        printf("\nEmail: ");
+        scanf("%99s", email);
+        printf("Senha: ");
+        scanf("%d", &senha);
+        //verificador login
+        if (strcmp(email, emailcad)==0 && senha == senhacad) {
+            printf("Seus dados estão corretos!");
+            break;
+        } else {
         printf("Seus dados estão incorretos!  Por favor, tente novamente!");
+        }
     }
-    }
-    return 0;
-}
-
-    //cadastro
 
     //carrinho de compra
     
@@ -64,7 +51,7 @@ int main()
     int pgto;
     float v1 = 0.0f;
     float v2 = 0.0f;
-    char cupom;
+    char cupom[20];
     float polo;
     float cbasica;
     float cjeans;
@@ -73,9 +60,9 @@ int main()
 
     printf("O valor total da compra foi de %.2f reais\n", v1);
     printf("Você possui cupom de desconto? S/N: ");
-    scanf(" %c", &cupom);
+    scanf(" %19s", cupom);
 
-    if (cupom == 'promo10' || cupom == 'PROMO10') {
+    if (strcmp(cupom, "promo10") == 0 || strcmp(cupom, "PROMO10") == 0) {
         v2 = v1 - (v1 * 0.10f);
         printf("O valor com desconto é de %.2f reais\n", v2);
     } else {
